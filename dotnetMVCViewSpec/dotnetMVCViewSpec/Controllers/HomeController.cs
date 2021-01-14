e75930674e5009305e0995f2690c35347d629370
horace.yeh <horaceyeh.web@gmail.com>
2021-01-14 23:39:30 +0800

    init View template and partialview

---
+++ dotnetMVCViewSpec/dotnetMVCViewSpec/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dotnetMVCViewSpec.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var temp =  Enumerable.Range(1, 5);
+            return View(temp);
+        }
+
+        public ActionResult Money()
+        {
+            return PartialView("_ShowMoneyPartial",999);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
